ba7b5fa277cd408203e0c0af5b97878bc673e749
Riordan-DC <riordancallil@outlook.com>
2018-11-28 13:05:17 +1000

    Implimenting the trajectory search with all its velocities within a 45 degree range

--- SeqSLAM-r/seqSLAM.py
+++ SeqSLAM-r/seqSLAM.py
@@ -148,22 +148,47 @@ def applyLocalContrast(frames):
 def trajectoryScore(row, column, v_step, ds):
     pass
 
+def getLine(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    x_coords = []
+    y_coords = []
+    for x in range(x1,x2):
+        y = y1 + dy * (x - x1) // dx
+        x_coords.append(x)
+        y_coords.append(y)
+    return x_coords,y_coords
+
 def trajectorySearch(matrix):
     rows,columns = np.shape(matrix)
     score_matrix = np.stack((np.empty([rows, columns]),)*3, axis=-1)
-    ds = 10
+    ds = 9
     v_steps = np.arange(1,10,1)
     display_matrix = np.stack((matrix,)*3, axis=-1)
     for column in range(columns):
-        image_scores = []
+        match_scores = []
         for row in range(rows):
             ds_ = int(ds/2)
-            chunk = matrix[row-ds_:row+ds_,column-ds_:column+ds_]
+            local_scores = [] #Chunk (match) local scores
+            chunk = matrix[max(0,row-ds_):row+ds_,max(0,column-ds_):column+ds_]
             #This is where I construct a local score for multiple velocities
-            
-            sequence = chunk.diagonal()
-            score = sum(sequence)
-            image_scores.append(score)
+            #score = np.trace(chunk)
+            x_line,y_line = getLine(0,0,np.shape(chunk)[0],np.shape(chunk)[1])
+            #print(x_line,y_line)
+            sequence = chunk[x_line, y_line]
+            score = np.sum(sequence)
+            local_scores.append(score)
+
+            #Naive line drawing algorithm
+            seen_sequence = [0,0,ds_,ds_]
+            right_corner = [ds_+1,ds_+1,ds,ds] 
+            #sequence = chunk[ds_]
+            #score = sum(sequence)
+            #local_scores.append(score)
+
+
+
+            match_scores.append(min(local_scores))
             '''
             #in construction
             for v_step in v_steps:
@@ -171,8 +196,8 @@ def trajectorySearch(matrix):
                 scores.append(score)
             '''
             cv2.imshow('trajectory search', score_matrix)
-        best_score = np.argmin(image_scores)
-        print(best_score)
+
+        best_score = np.argmin(match_scores)
         score_matrix[best_score][column] = 1
         #display_matrix = cv2.circle(display_matrix, (column, row), 1, (0,255,0), -1)
         #matrix = cv2.circle(matrix, (row, column), 3, (0,255,0), -1)
